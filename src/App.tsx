09dbb5c80ee0ded037f73c8cec64e1369bc8071c
PedroTakahashi <pedro.takahashi@objective.com.br>
2022-01-21 15:23:06 -0300

    template pronto

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../src/Assets/styles/base.css'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header" />
+      <h2>começa aqui</h2>
+    </div>
+  );
+}
+
+export default App;
